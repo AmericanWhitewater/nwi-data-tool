38eae44b5e0a7d3c0af8fee89b94cbab579b9df2
Seth Fitzsimmons <seth@mojodna.net>
2019-05-21 12:23:55 -0700

    Include additional attributes when exporting

--- sql/descriptive_reach_segments.sql
+++ sql/descriptive_reach_segments.sql
@@ -3,9 +3,12 @@ CREATE VIEW descriptive_reach_segments AS
     reach_id,
     river,
     section,
+    altname,
     huc4,
     review,
     review_reason,
+    class,
+    abstract,
     reach_segments.geom
   FROM reach_segments
   JOIN snapped_putins USING (reach_id)
